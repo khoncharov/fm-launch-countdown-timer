cb742c0a2db998948f787ff26d283149c6932115
Konstantin Honcharov <k.honcharov@gmail.com>
2023-01-27 20:15:51 +0300

    feat: add ids

---
+++ src/app/const.ts
@@ -0,0 +1,29 @@
+export enum DigitNumId {
+  DAY_TOP_FIX = 'day-top-fix',
+  DAY_TOP_FLIP = 'day-top-flip',
+  DAY_BOTTOM_FIX = 'day-bottom-fix',
+  DAY_BOTTOM_FLIP = 'day-bottom-flip',
+  HOUR_TOP_FIX = 'hour-top-fix',
+  HOUR_TOP_FLIP = 'hour-top-flip',
+  HOUR_BOTTOM_FIX = 'hour-bottom-fix',
+  HOUR_BOTTOM_FLIP = 'hour-bottom-flip',
+  MINUTE_TOP_FIX = 'minute-top-fix',
+  MINUTE_TOP_FLIP = 'minute-top-flip',
+  MINUTE_BOTTOM_FIX = 'minute-bottom-fix',
+  MINUTE_BOTTOM_FLIP = 'minute-bottom-flip',
+  SECOND_TOP_FIX = 'second-top-fix',
+  SECOND_TOP_FLIP = 'second-top-flip',
+  SECOND_BOTTOM_FIX = 'second-bottom-fix',
+  SECOND_BOTTOM_FLIP = 'second-bottom-flip',
+}
+
+export enum DigitFlipId {
+  DAY_TOP = 'day-top',
+  DAY_BOTTOM = 'day-bottom',
+  HOUR_TOP = 'hour-top',
+  HOUR_BOTTOM = 'hour-bottom',
+  MINUTE_TOP = 'minute-top',
+  MINUTE_BOTTOM = 'minute-bottom',
+  SECOND_TOP = 'second-top',
+  SECOND_BOTTOM = 'second-bottom',
+}
