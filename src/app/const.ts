1c9f273f3565a2ae4fe00c645e7a1397db8f2a07
Konstantin Honcharov <k.honcharov@gmail.com>
2023-01-29 04:09:50 +0300

    feat: add const

--- src/app/const.ts
+++ src/app/const.ts
@@ -1,15 +1,26 @@
-import { TimeShiftObject } from './types';
-
 export const TIME_CHECK_TIMEOUT = 50;
+
+export const MAX_DAYS = 99;
+export const MAX_HOURS = 23;
+export const MAX_MINUTES = 59;
+export const MAX_SECONDS = 59;
+export const MIN_VALUE = 0;
 
 export const MS_PER_DAY = 1000 * 60 * 60 * 24;
 export const MS_PER_HOUR = 1000 * 60 * 60;
 export const MS_PER_MINUTE = 1000 * 60;
 export const MS_PER_SECOND = 1000;
 
-export const DEFAULT_TIME_SHIFT: TimeShiftObject = {
-  days: 0, // 8
-  hours: 0, // 23
-  minutes: 1, // 56
-  seconds: 0, // 0
+export interface TimeShift {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+export const DEFAULT_TIME_SHIFT: TimeShift = {
+  days: 8,
+  hours: 23,
+  minutes: 56,
+  seconds: 0,
 };
